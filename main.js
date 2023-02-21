37996450d7c36a2d2b6439456b8601c9eb287b05
agospulenserra <agostinapulenserra@gmail.com>
2023-02-21 15:20:15 -0300

    'version3'

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 // clases
 
 
-class Usuario{
+class Alumno{
 
     constructor(nombre, contrasenia){
 
         this.nombre = nombre;
         this.contrasenia = contrasenia;
-        this.caracteristica = ''
+        this.caracteristica = 'alumno'
+
+    }
+
+}
+
+class Profesor{
+
+    constructor(nombre, contrasenia){
+
+        this.nombre = nombre;
+        this.contrasenia = contrasenia;
+        this.caracteristica = 'profesor'
 
     }
 
@@ -18,21 +30,41 @@ class Usuario{
 
 
 //seccion registro
-function eventoSubmitRegistro (nombre, contrasenia){
+function eventoSubmitRegistroProfesor (nombre, contrasenia){
 
     //creo el usuario y lo agrego a la lista
-    usuarios.push(new Usuario(nombre, contrasenia))
-    console.log(usuarios)
+    profesores.push(new Profesor(nombre, contrasenia))
 
-    localStorage.setItem("lista_de_usuarios", JSON.stringify(usuarios));
+    localStorage.setItem("lista_de_profesores", JSON.stringify(profesores));
 
-    //vacio los inputs
-    nombre.value = ""
-    contrasenia.value = ""
+    guardarEnLSALosUsuarios()
 
 }
 
-function registro (){
+function eventoSubmitRegistroAlumno (nombre, contrasenia){
+
+    //creo el usuario y lo agrego a la lista
+    alumnos.push(new Alumno(nombre, contrasenia))
+
+    localStorage.setItem("lista_de_alumnos", JSON.stringify(alumnos))
+
+    guardarEnLSALosUsuarios()
+
+}
+
+function guardarEnLSALosUsuarios (){
+
+    //concateno los arrays en el array usuarios y lo guardo en el LS
+    usuarios = profesores.concat(alumnos)
+
+    localStorage.setItem("lista_de_usuarios", JSON.stringify(usuarios));
+
+}
+
+function registroProfesor (event){
+
+    //freno el flow del programa
+    event.preventDefault();
 
     //limpio el contenedor principal
     contenedorPrincipal.innerHTML = " "
@@ -56,7 +88,7 @@ function registro (){
         const nombre = inputNombreRegistro.value
         const contrasenia = inputContraseniaRegistro.value
 
-        eventoSubmitRegistro(nombre, contrasenia)
+        eventoSubmitRegistroProfesor(nombre, contrasenia)
 
     })
 
@@ -68,18 +100,65 @@ function registro (){
 
 }
 
+function registroAlumno (event){
+
+    //freno el flow del programa
+    event.preventDefault();
+
+    //limpio el contenedor principal
+    contenedorPrincipal.innerHTML = " "
+
+    //creo los elementos
+    const divContenedorRegistro = document.createElement('div')
+    divContenedorRegistro.className = 'contenedorInicio'
+
+    const inputNombreRegistro = document.createElement('input')
+    inputNombreRegistro.type = 'text'
+    inputNombreRegistro.placeholder = 'Crea tu usuario'
+
+    const inputContraseniaRegistro = document.createElement('input')
+    inputContraseniaRegistro.type = 'password'
+    inputContraseniaRegistro.placeholder = 'Crea tu contrasenia'
+
+    const submitUsuarioRegistro = document.createElement('button')
+    submitUsuarioRegistro.innerHTML = 'Registrarme'
+    submitUsuarioRegistro.addEventListener('click', () =>{
+
+        const nombre = inputNombreRegistro.value
+        const contrasenia = inputContraseniaRegistro.value
+
+        eventoSubmitRegistroAlumno(nombre, contrasenia)
+
+    })
+
+    //agrego los elementos al div
+    divContenedorRegistro.append(inputNombreRegistro, inputContraseniaRegistro, submitUsuarioRegistro)
+
+    //agrego el div al contenedor
+    contenedorPrincipal.append(divContenedorRegistro)
+
+}
+
+
 //seccion login
 function obtenerUsuariosEnLS (){
 
-    let usuarioLS = localStorage.getItem("lista_de_usuarios")
+    let alumnoLS = localStorage.getItem("lista_de_alumnos")
+    let profesorLS = localStorage.getItem("lista_de_profesores")
 
-    if (usuarioLS !== null){
+    if (alumnoLS !== null){
 
-        usuariosEnLS = JSON.parse(usuarioLS)
+        alumnosEnLS = JSON.parse(alumnoLS)
 
     }
 
-    return usuariosEnLS
+    if (profesorLS !== null){
+
+        profesoresEnLS = JSON.parse(profesorLS)
+
+    }
+
+    return alumnosEnLS, profesoresEnLS
 
 
 }
@@ -87,15 +166,16 @@ function obtenerUsuariosEnLS (){
 function buscarUsuarioIngresadoEnElLS (nombreDelLogin, contraDelLogin){
 
     //veo si existe el usuario en el local storage
-    const indiceUsuario = usuariosEnLS.find((usuario) => (usuario.nombre === nombreDelLogin) && (usuario.contrasenia === contraDelLogin))
+    const indiceAlumno = alumnosEnLS.find((usuario) => (usuario.nombre === nombreDelLogin) && (usuario.contrasenia === contraDelLogin));
+    const indiceProfesor = profesoresEnLS.find((usuario) => (usuario.nombre === nombreDelLogin) && (usuario.contrasenia === contraDelLogin));
 
     //si existe lo mando a otra pagina
     //indiceUsuario !== undefined? window.location = 'main.html' : errorEnElLogin()
-    if (indiceUsuario !== undefined && usuariosEnLS.caracteristica === 'alumno'){
+    if (indiceAlumno !== undefined){
 
         window.location = 'paginaAlumno.html'
 
-    }else if ((indiceUsuario !== undefined && usuariosEnLS.caracteristica === 'profesor')){
+    }else if (indiceProfesor !== undefined){
 
         window.location = 'paginaProfesor.html'
 
@@ -109,7 +189,7 @@ function buscarUsuarioIngresadoEnElLS (nombreDelLogin, contraDelLogin){
 }
 
 
-function login (event){
+function loginProfesor (event){
 
     //feno el flow
     event.preventDefault();
@@ -119,7 +199,7 @@ function login (event){
 
     //creo los elementos
     const divContenedor = document.createElement('div')
-    divContenedor.id = 'contenedorLogin'
+    divContenedor.className = 'contenedorInicio'
 
     const inputNombre = document.createElement('input')
     inputNombre.type = 'text'
@@ -145,12 +225,112 @@ function login (event){
     
     })
 
+    const divRegistro = document.createElement('div')
+    divRegistro.className = 'divRegistro'
+
+    const btnRegistro = document.createElement('p')
+    btnRegistro.innerHTML = 'Registrate aca'
+    btnRegistro.className = 'aPaginaPrincipal'
+    btnRegistro.addEventListener('click', (e) => {
+        registroProfesor(e)
+    })
+    
+
+    //agrego btnRegistro al divRegistro
+    divRegistro.append(btnRegistro)
+
     //agrego los elementos al div
-    divContenedor.append(inputNombre, inputContrasenia, submitUsuario)
+    divContenedor.append(inputNombre, inputContrasenia, submitUsuario, divRegistro)
 
     //agrego el div al contenedor
     contenedorPrincipal.append(divContenedor)
 }
+
+function loginAlumno (event){
+
+    //feno el flow
+    event.preventDefault();
+
+    //limpio el contenedor principal
+    contenedorPrincipal.innerHTML = " "
+
+    //creo los elementos
+    const divContenedor = document.createElement('div')
+    divContenedor.className = 'contenedorInicio'
+
+    const inputNombre = document.createElement('input')
+    inputNombre.type = 'text'
+    inputNombre.placeholder = 'Usuario'
+
+    const inputContrasenia = document.createElement('input')
+    inputContrasenia.type = 'password'
+    inputContrasenia.placeholder = 'Contrasenia'
+
+    const submitUsuario = document.createElement('button')
+    submitUsuario.innerHTML = 'Ingresar'
+    submitUsuario.addEventListener('click', (event) =>{
+
+        //freno el flow del input
+        event.preventDefault();
+    
+        //obtengo los datos de los inputs
+        const nombreDelLogin = inputNombre.value
+        const contraDelLogin = inputContrasenia.value
+    
+        obtenerUsuariosEnLS()
+        buscarUsuarioIngresadoEnElLS(nombreDelLogin, contraDelLogin) 
+    
+    })
+
+    const divRegistro = document.createElement('div')
+    divRegistro.className = 'divRegistro'
+
+    const btnRegistro = document.createElement('p')
+    btnRegistro.innerHTML = 'Registrate aca'
+    btnRegistro.className = 'aPaginaPrincipal'
+    btnRegistro.addEventListener('click', (e) => {
+        registroAlumno(e)
+    })
+    
+
+    //agrego btnRegistro al divRegistro
+    divRegistro.append(btnRegistro)
+
+    //agrego los elementos al div
+    divContenedor.append(inputNombre, inputContrasenia, submitUsuario, divRegistro)
+
+    //agrego el div al contenedor
+    contenedorPrincipal.append(divContenedor)
+}
+
+//seccion API
+async function llamadoALaAPI (divProfesor, divAlumno){
+
+    //obtengo los personajes
+    const response = await fetch("https://rickandmortyapi.com/api/character/[1,2]");
+    const responseJSON = await response.json();
+    const personajesInicio = responseJSON.results;
+
+    //recorro los personajes y genero los agrego al inicio
+    for (const personaje of personajesInicio){
+
+        //creo el div
+        const divPersonaje = document.createElement('div');
+
+        //creo la imagen de los personajes
+        const imagenPersonaje = document.createElement('img');
+        imagenPersonaje.src = personaje.image;
+
+        //los agrego a sus divs correspondientes
+        divPersonaje.append(imagenPersonaje);
+
+        divProfesor.append(divPersonaje);
+        divAlumno.append(divPersonaje);
+
+    }
+
+}
+
 
 //inicio del programa
 function inicioDelPrograma (){
@@ -163,13 +343,13 @@ function inicioDelPrograma (){
     const divProfesor = document.createElement('div')
     divProfesor.id = 'divProfesor'
     divProfesor.addEventListener('click', (e) => {
-        login(e)
+        loginProfesor(e)
     })
 
     const divAlumno = document.createElement('div')
     divAlumno.id = 'divAlumno'
     divAlumno.addEventListener('click', (e) =>{
-        login(e)
+        loginAlumno(e)
     })
     
     const textoProfesor = document.createElement('p')
@@ -184,9 +364,10 @@ function inicioDelPrograma (){
     divProfesor.append(textoProfesor)
     divAlumno.append(textoAlumno)
 
+    llamadoALaAPI(divProfesor, divAlumno)
+
     //los agrego al contenedor
     contenedor.append(divProfesor, divAlumno)
-
 
 }
 
@@ -194,11 +375,12 @@ function inicioDelPrograma (){
 
 // programa
 
+
 const contenedorPrincipal = document.getElementById('contenedorPrincipal')
-const btnRegistro = document.getElementById('btnRegistro')
-btnRegistro.addEventListener('click', registro())
-let usuarios = []
-let usuariosEnLS = []
+let alumnosEnLS = []
+let profesoresEnLS = []
+let alumnos = []
+let profesores = []
 
 inicioDelPrograma()
 
